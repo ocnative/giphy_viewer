9bed52e8a9dd7bc9811e0aec6cfd471d264ab081
Bill <lrpc949@gmail.com>
2017-11-21 13:25:35 -0800

    js commit with comments

--- hw.js
+++ hw.js
@@ -1,38 +1,64 @@
+// From learn.jquery.com:
+  // A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you.
+  //  Code included inside $(document ).ready() will only run once the page DOM is ready for JavaScript code to execute.
+  // This is an anonymous function because there is no valur inside the () of function()
+  // callback function will call back .ready
 $(document).ready(function() {
 
+// Create your array of animals already on the HTML page when it loads.
   var animals = [
-    "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
-    "bird", "ferret", "turtle", "sugar glider", "chinchilla",
-    "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
-    "capybara", "teacup pig", "serval", "salamander", "frog"
+    "dog", "cat", "kitten", "puppy", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara",
+// BIG QUESTIONS:
+//When I add "teacup pig", etc. after "serval", the items after serval are white, not green. This is also true if I switch places between serval and teacup pig.
+    "serval",
+    "teacup pig", "salamander", "frog"
   ];
 
-  // function to make buttons and add to page
-  function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+// Create buttons, add them to HTML page.
+  //  Parameters:
+    //  arrayToUse: The array of your animals.
+    //  classToAdd: The class used for event handlers. In this case, it links the code to any button clicks through any element using class = classToAdd.
+    //  areaToAddTo:  Defines where on the page to place newly created buttons.
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    // $(areaToAddTo) is a jQuery method.
+    // Here we use it to define where on the page to place newly created buttons.
+    // Each time tthe next line runs, it clears the child elements from the element with ID equal to areaToAddTo.
     $(areaToAddTo).empty();
 
-    for (var i = 0; i < arrayToUse.length; i++) {
-      var a = $("<button>");
-      a.addClass(classToAdd);
-      a.attr("data-type", arrayToUse[i]);
-      a.text(arrayToUse[i]);
-      $(areaToAddTo).append(a);
-    }
+    // arrayToUse gets defined as the variable "animals".
+      for (var i = 0; i < arrayToUse.length; i++) {
+    // var a is jQuery shorthand for document.createElement ("button")
+        var a = $("<button>");
+    // These methods("a.addClass", etc.) are jQuery shorthand methods available on object "a" because we used the $ syntax when creating var a above.
+        a.addClass(classToAdd);
+        // Sets new attribute "data-type" to "animals", which is the "arrayToUse".
+        a.attr("data-type", arrayToUse[i]);
+        // Sets new attribute "text" to the current index value of "animals", which is the "arrayToUse".
+        a.text(arrayToUse[i]);
+        // This appends a new button (value of var a) into the element specified by areaToAddTo.
+        $(areaToAddTo).append(a);
+      }
 
   }
-
+  // In ".animal-button" the . denotes that "animal button" is a class.
   $(document).on("click", ".animal-button", function() {
+    // #animals tells us that "animals" is a unique element ID.
     $("#animals").empty();
     $(".animal-button").removeClass("active");
     $(this).addClass("active");
 
+    // "$(this)" is the currently clicked button.
     var type = $(this).attr("data-type");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+    // You can type http://api.giphy.com/v1/gifs/search?q=squirrel&api_key=dc6zaTOxFJmzC&limit=12 into a browser address bar and it will return the data for your GIFs instead of the GIFs themselves.
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=12";
 
+// jQuery shorthand for the ajax method to request for the URL that is the vaue of the var queryURL defined above.
     $.ajax({
       url: queryURL,
       method: "GET"
     })
+
+// ".done" is a jQuery method. It allows you to attach multiple handlers before or after it's processed and they will all execute. This prevents you from having to squeeze everything into a single function.
     .done(function(response) {
       var results = response.data;
       console.log(results)
@@ -61,6 +87,7 @@ $(document).ready(function() {
     });
   });
 
+  // Change from Still to Animate or vice versa.
   $(document).on("click", ".animal-image", function() {
 
     var state = $(this).attr("data-state");
@@ -75,6 +102,7 @@ $(document).ready(function() {
     }
   });
 
+  // QUESTIONS: How do I delete an animal?
   $("#add-animal").on("click", function(event) {
     event.preventDefault();
     var newAnimal = $("input").eq(0).val();
@@ -82,10 +110,10 @@ $(document).ready(function() {
     if (newAnimal.length > 2) {
       animals.push(newAnimal);
     }
-
+// Reflls the buttons on the page. (We called "empty" on $(areaToAddTo) way at the top.
     populateButtons(animals, "animal-button", "#animal-buttons");
 
   });
-
+// QUESTIONS:I know that this is the same command as above, and I know that they are in different scopes. Why?
   populateButtons(animals, "animal-button", "#animal-buttons");
 });
